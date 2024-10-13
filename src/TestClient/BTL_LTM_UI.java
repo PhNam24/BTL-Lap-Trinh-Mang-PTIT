34179aa06eb1168b5edf6dcc42f189cb92f7621f
PhNam24 <phamnam240803@gmail.com>
2024-10-13 23:11:38 +0700

    update smt

---
+++ src/TestClient/BTL_LTM_UI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package TestClient;
+
+/**
+ *
+ * @author ACER
+ */
+public class BTL_LTM_UI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
